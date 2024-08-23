da392a7e2065194a25fab5bceb48fbce86431a7f
DiegoSerrano1607 <diego.eserrano16@gmail.com>
2024-08-22 21:35:51 -0600

    Subida de ejemplo

---
+++ src/vista/Main.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package vista;
+import modelo.conexion; 
+
+/**
+ *
+ * @author Diego Serrano
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        conexion cn = new conexion();
+        cn.abrir_conexion();
+        cn.cerrar_conexion();
+        //frm_persona frm = new frm_persona();
+        //frm.show();
+    }
+    
+}
